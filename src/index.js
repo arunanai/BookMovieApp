87ac708ca1962ee225bade5537d71e46fbb12e72
aruna <aruna.n1504@gmail.com>
2021-07-05 19:42:37 -0500

    move controller component to screens folder and update the path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
-import Controller from './Controller';
+import Controller from './screens/Controller';
 
 ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
